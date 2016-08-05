9d56313270b3f0e4fb3422e5f1f547b467e49283
dany92 <dany@bc.edu>
2016-08-05 07:33:41 -0400

    begin routing

---
+++ server/app/routes/events/index.js
@@ -0,0 +1,47 @@
+var router = require('express').Router();
+var db = require('../../../../server/db');
+var Event = db.model('event');
+module.exports = router;
+
+
+router.get('/', function(req,res,next){
+	Event.findAll({where: req.query})
+	.then(events => res.json(events))
+	.catch(next);
+})
+
+router.param('id', function (req, res, next, id) {
+	Event.findById(id)
+	.then(function (event) {
+		if (event) {
+			req.event = event;
+			next();
+			return null; 
+		} else {
+			throw HttpError(404);
+		}
+	})
+	.catch(next);
+});
+
+router.get('/:id', function(req,res,next){
+	res.json(req.event);
+})
+
+router.post('/', function(req,res,next){
+	Review.create(req.body)
+	.then(createdEvent => res.json(createdEvent))
+	.catch(next);
+})
+
+router.put('/:id', function(req,res,next){
+	req.event.update(req.body)
+	.then(updatedEvent => res.json(updatedEvent))
+	.catch(next);
+})
+
+router.delete('/:id', function(req,res,next){
+	req.event.destroy()
+	.then(destroyedEvent => res.send('Event Destroyed'))
+	.catch(next);
+})
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,7 +2,16 @@
 var router = require('express').Router();
 module.exports = router;
 
-router.use('/members', require('./members'));
+
+// router.use('/users', require('./users'));
+// router.use('/members', require('./members'));
+// router.use('/businesses', require('./businesses'));
+router.use('/events', require('./events'));
+// router.use('/feeds', require('./feeds'));
+// router.use('/promotions', require('./promotions'));
+
+
+
 
 // Make sure this is after all of
 // the registered routes!
