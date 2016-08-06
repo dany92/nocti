c98a63e2707a876196f91a6ad4fabb550ddf1e76
dany92 <dany@bc.edu>
2016-08-06 16:52:24 -0400

    the event search works

--- browser/js/common/controllers/SearchController.js
+++ browser/js/common/controllers/SearchController.js
@@ -5,22 +5,43 @@ app.controller('SearchCtrl', function($scope, BusinessFactory, EventFactory){
 	BusinessFactory.fetchAll()
 	.then(businesses => {
 		console.log("business in factory", businesses);
-		bTotal = businesses;
-		$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15, bTotal.within20);
+		bTotal = {
+			within5: businesses.within5 || [],
+			within10: businesses.within10 || [],
+			within15: businesses.within15 || [],
+			within20: businesses.within20 || []
+		}
+		$scope.businesses = [];
+		for(var group in bTotal){
+			$scope.businesses = $scope.businesses.concat(bTotal[group]);
+		}
+		setEvents();
 		$scope.$watch('limit', function(){
 			if($scope.limit === 5){
 				$scope.businesses = bTotal.within5;
+				setEvents();
 			}else if($scope.limit === 10){
 				$scope.businesses = bTotal.within5.concat(bTotal.within10);
+				setEvents();
 			}else if ($scope.limit === 15){
 				$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15);
+				setEvents();
 			}else{
 				$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15, bTotal.within20);
+				setEvents();
 			}
 		})
+
 	})
 
-	EventFactory.fetchAll()
-	.then(events => $scope.events = events)
+	function setEvents(){
+		var businessIds = $scope.businesses.map(b=>b.id)
+		EventFactory.fetchAll({businessId:
+			businessIds
+		})
+		.then(function(events){
+			$scope.events = events;
+		})
+	}
 
 })
