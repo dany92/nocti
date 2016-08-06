d9a912d0ac0f49e5904d8b5c7ee578c26df57848
dany92 <dany@bc.edu>
2016-08-06 15:45:31 -0400

    got rid of the error message can't concat

--- browser/js/common/controllers/SearchController.js
+++ browser/js/common/controllers/SearchController.js
@@ -7,18 +7,17 @@ app.controller('SearchCtrl', function($scope, BusinessFactory, EventFactory){
 		console.log("business in factory", businesses);
 		bTotal = businesses;
 		$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15, bTotal.within20);
-	})
-
-	$scope.$watch('limit', function(){
-		if($scope.limit === 5){
-			$scope.businesses = bTotal.within5;
-		}else if($scope.limit === 10){
-			$scope.businesses = bTotal.within5.concat(bTotal.within10);
-		}else if ($scope.limit === 15){
-			$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15);
-		}else{
-			$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15, bTotal.within20);
-		}
+		$scope.$watch('limit', function(){
+			if($scope.limit === 5){
+				$scope.businesses = bTotal.within5;
+			}else if($scope.limit === 10){
+				$scope.businesses = bTotal.within5.concat(bTotal.within10);
+			}else if ($scope.limit === 15){
+				$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15);
+			}else{
+				$scope.businesses = bTotal.within5.concat(bTotal.within10, bTotal.within15, bTotal.within20);
+			}
+		})
 	})
 
 	EventFactory.fetchAll()
