f1734a7394cb937063a81f9e64166d76ea2e992f
dany92 <dany@bc.edu>
2016-08-07 19:37:11 -0400

    updated manage business

--- browser/js/common/states/manage-business/events/events.js
+++ browser/js/common/states/manage-business/events/events.js
@@ -8,17 +8,6 @@ app.config(function ($stateProvider) {
                 return EventFactory.fetchAll({businessId: $stateParams.id});
             }
         },
-        controller: function($scope, events, EventFactory, $stateParams){
-        	$scope.events = events;
-            $scope.events.past = events.filter(e=> e.isPast);
-            $scope.events.upcoming = events.filter(e=> !e.isPast);
-            var businessId = $stateParams.id
-            $scope.submit = function(){
-                $scope.newEvent.businessId = businessId;
-                EventFactory.createEvent($scope.newEvent)
-                .then(created=>$scope.events.upcoming.push(created))
-            }
-        }
+        controller: 'EventMangerCtrl'
     })
-    
 })
